6b163293ffe022c350993c7fd29909c78d878a8c
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2021-12-22 14:19:10 +0600

    script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ var example1 = 10 === 10; //equals
 var example2 = 11 > 10; // more than 
 var example3 = 10 < 11; //less than
 var example4 = 10 >= 10; //more than or equals
+var example5 = 10 <= 10; //less than or equals
+var example6 = 10 !== 10; //not equals
+
+var a = 5;
+var b = 10;
+var example7 = a < b;
