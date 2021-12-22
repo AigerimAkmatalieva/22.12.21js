41cb33a80d78418b3681a87d14495d80db3490ae
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2021-12-22 15:52:03 +0600

    script.js

--- script.js
+++ script.js
@@ -5,44 +5,11 @@ var example4 = 10 >= 10; //more than or equals
 var example5 = 10 <= 10; //less than or equals
 var example6 = 10 !== 10; //not equals
 
-var a = 5;
-var b = 10;
-var example7 = a < b;
 
-var c = 10;
-var d = 11;
-if (c < d) {
-  alert("C is less than D");
-  console.log('Hello World');
-}
-
-var age = 10;
-if (age >= 18) {
-  alert("You acn vote!");
+let user_otvet = prompt("Где вы живете?");
+if (user_otvet == "Каракол") {
+  alert("Ответ верный!");
 }
 else {
-  alert("You are too young to note");
+  alert("Ответ неверный!");
 }
-
-var lightSwitch = 1;
-if (lightSwitch) {
-  alert("Light is on");
-}
-else {
-  alert("Light is off");
-}
-
-if (confirm("Do you have a car?")) {
-  alert("then this website is not for you!")
-}
-else {
-  alert("Welcome fellow pedestrian!")
-}
-var username = prompt("What is your name?");
-alert("Welcome" + username);
-
-var a = promt("What the width of the rectangle?");
-var a = promt("What the width of the rectangle?");
-alert(a + b);
-
-
